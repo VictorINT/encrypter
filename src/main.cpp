35be30c68a4d8d9103508e045e7eaa413efca8fb
Rares Vanca <raresvanca@gmail.com>
2021-02-03 16:09:01 +0200

    README.md

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ int main(){
 
     auto* key = (unsigned char *) skey.c_str();
     auto* input = (unsigned char *) plainText.c_str();
-    auto* main = new Encrypter(128);
+    auto* main = new Encrypter(256);
 
     if (mode == "e" || mode == "encryption"){
         cout << "\n================Encrypted text=================\n" << toHex(reinterpret_cast<char*>(main->encrypt(input, strlen(reinterpret_cast<const char *>(input)) * sizeof(unsigned char), key))) << endl;
